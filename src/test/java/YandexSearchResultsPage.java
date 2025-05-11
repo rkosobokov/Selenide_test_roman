4940cb05947c5f353e875df0211d8659b6bf10c6
rkosobokov <itkosobokov@yandex.ru>
2025-05-11 10:35:54 +0300

    задание модуля 12 (после замечания) PageOpject, PageElements классы BaseTest, YandexMainPage, YandexSearchResultsPage, YandexTest

--- src/test/java/YandexSearchResultsPage.java
+++ src/test/java/YandexSearchResultsPage.java
@@ -1,22 +1,10 @@
 import com.codeborne.selenide.SelenideElement;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
-
 public class YandexSearchResultsPage {
-    public SelenideElement logoLink() {
-        return $x("/html/body/div[1]/div[1]/header/form/div[1]/a");
-    }
-
-    public SelenideElement logo() {
-        return logoLink().$("svg");
-    }
-
-    public SelenideElement firstPath() {
-        return logo().$("path:nth-child(1)");
-    }
-
-    public SelenideElement secondPath() {
-        return logo().$("path:nth-child(2)");
-    }
+    // Объявляем элементы как поля класса
+    public SelenideElement logoLink = $x("/html/body/div[1]/div[1]/header/form/div[1]/a");
+    public SelenideElement logo = logoLink.$("svg");
+    public SelenideElement firstPath = logo.$("path:nth-child(1)");
+    public SelenideElement secondPath = logo.$("path:nth-child(2)");
 }
--- src/test/java/YandexTest.java
+++ src/test/java/YandexTest.java
@@ -1,3 +1,4 @@
+import org.apache.commons.lang3.ObjectUtils;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import static com.codeborne.selenide.Condition.*;
@@ -15,9 +16,9 @@ public class YandexTest extends BaseTest {
     @Step("Открыть Яндекс, ввести 'Selenide' в поисковую строку и отменить предложение о установке Яндекса по умолчанию")
     void searchTest() {
         open("https://ya.ru");
-        yandexMainPage.searchInput().shouldBe(visible);
+        yandexMainPage.searchInput.shouldBe(visible);
         yandexMainPage.searchFor("Selenide");
-        yandexMainPage.declineButton().click();
+        yandexMainPage.declineButton.click();
     }
 
     @Test
@@ -25,32 +26,29 @@ public class YandexTest extends BaseTest {
     @DisplayName("Получение всех атрибутов и проверка цвета значка яндекс на красный #F8604A")
     @Step("Получение всех атрибутов и проверка цвета значка яндекс на красный #F8604A")
     void getAllAttributes() {
-        // Получаем отдельные атрибуты
         System.out.println("Атрибуты элемента:");
-        System.out.println("href = " + searchResultsPage.logoLink().attr("href"));
-        System.out.println("class = " + searchResultsPage.logoLink().attr("class"));
-        System.out.println("aria-label = " + searchResultsPage.logoLink().attr("aria-label"));
-        System.out.println("data-counter = " + searchResultsPage.logoLink().attr("data-counter"));
-        System.out.println("data-log-node = " + searchResultsPage.logoLink().attr("data-log-node"));
+        System.out.println("href = " + searchResultsPage.logoLink.attr("href"));
+        System.out.println("class = " + searchResultsPage.logoLink.attr("class"));
+        System.out.println("aria-label = " + searchResultsPage.logoLink.attr("aria-label"));
+        System.out.println("data-counter = " + searchResultsPage.logoLink.attr("data-counter"));
+        System.out.println("data-log-node = " + searchResultsPage.logoLink.attr("data-log-node"));
 
-        // Получаем атрибуты вложенного SVG
         System.out.println("\nАтрибуты SVG:");
-        System.out.println("xmlns = " + searchResultsPage.logo().attr("xmlns"));
-        System.out.println("width = " + searchResultsPage.logo().attr("width"));
-        System.out.println("height = " + searchResultsPage.logo().attr("height"));
-        System.out.println("fill = " + searchResultsPage.logo().attr("fill"));
-        System.out.println("viewBox = " + searchResultsPage.logo().attr("viewBox"));
+        System.out.println("xmlns = " + searchResultsPage.logo.attr("xmlns"));
+        System.out.println("width = " + searchResultsPage.logo.attr("width"));
+        System.out.println("height = " + searchResultsPage.logo.attr("height"));
+        System.out.println("fill = " + searchResultsPage.logo.attr("fill"));
+        System.out.println("viewBox = " + searchResultsPage.logo.attr("viewBox"));
 
-        // Получаем атрибуты path элементов
         System.out.println("\nАтрибуты первого path:");
-        System.out.println("fill = " + searchResultsPage.firstPath().attr("fill"));
-        System.out.println("d = " + searchResultsPage.firstPath().attr("d"));
+        System.out.println("fill = " + searchResultsPage.firstPath.attr("fill"));
+        System.out.println("d = " + searchResultsPage.firstPath.attr("d"));
 
         System.out.println("\nАтрибуты второго path:");
-        System.out.println("fill = " + searchResultsPage.secondPath().attr("fill"));
-        System.out.println("d = " + searchResultsPage.secondPath().attr("d"));
+        System.out.println("fill = " + searchResultsPage.secondPath.attr("fill"));
+        System.out.println("d = " + searchResultsPage.secondPath.attr("d"));
 
-        // Проверяем, что атрибут fill равен #F8604A (Красный)
-        searchResultsPage.firstPath().shouldHave(attribute("fill", "#F8604A"));
+        searchResultsPage.firstPath.shouldHave(attribute("fill", "#F8604A"));
     }
+
 }
